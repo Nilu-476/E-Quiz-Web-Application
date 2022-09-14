28041f93e32ecb70a87094bb25a6bb6eeefde1f0
BharteshGirmal <bharteshgirmal@gmail.com>
2022-09-14 11:18:51 +0530

    React Js

--- FrontEnd/e_quiz/src/Components/HomeComponent/Homepage.js
+++ FrontEnd/e_quiz/src/Components/HomeComponent/Homepage.js
@@ -5,7 +5,7 @@ export default function Homepage()
  
       
         return(
-      <body className="main">
+      <body className="main col-xl-12">
       <div className="header">
             <h1>E-Quiz Web Application</h1>
       </div>
@@ -28,33 +28,61 @@ export default function Homepage()
                 <div className="main" align="center">
             <img src="https://cdn.images.express.co.uk/img/dynamic/130/590x/Easy-quiz-questions-1282929.jpg?r=1621866486163" alt="" />
             </div>
-      <div className='row align-items-center"'>
-      <div className='col-xs-6 col-sm-4 bg-info text-dark lh-lg'>
-            <div><h2>About us</h2></div>
+      <div className='row  lh-lg' >
+      <div className='col-xs-6 col-sm-4 text-dark'>
+      <div className="card  bg-info " style={{width: "25rem"}}>
+      <img src="https://www.freeiconspng.com/thumbs/about-us-icon/information-about-us-icon-10.png" class="card-img-top" style={{width:"300px",height:"300px",alignSelf:"center"}} alt="..."/>
+      <div className="card-body">
+      <h5 className="card-title"><h2>About us</h2></h5>
+      <p className="card-text">
             <h3>
-            In this E-Quiz application the test can be arranged in a different types based on the subjects 
-	      the Resistered teacher/user can create a test of subject accordingly , 
+            teacher/user can create a test of subject accordingly ,
             he can get the results of all students after test complete
-	      the Registered student can access the test and can track his performance of test of all subjects shown by the chart 
+            the Registered student can access the test and can track his 
+            performance of test of all subjects shown by the chart
             </h3>
-      </div>
-      <div className='col-xs-6 col-sm-4 bg-success text-dark lh-lg'>
-            <div><h2>Faculty</h2></div>
+            </p>
+      <a href="#" className="btn btn-primary">Go somewhere</a>
+  </div>
+</div>
+</div>
+<div className='col-xs-6 col-sm-4 text-dark'>
+      <div className="card  bg-info " style={{width: "25rem"}}>
+      <img src="https://static.ess.com/uploads/Teaching-Icon.png" class="card-img-top" style={{width:"300px",height:"300px",alignSelf:"center"}} alt="..."/>
+      <div className="card-body">
+      <h5 className="card-title"><h2>Teacher</h2></h5>
+      <p className="card-text">
             <h3>
-           teacher/user can create a test of subject accordingly ,
+            teacher/user can create a test of subject accordingly ,
             he can get the results of all students after test complete
+            the Registered student can access the test and can track his 
+            performance of test of all subjects shown by the chart
             </h3>
-      </div>
-      <div className='col-xs-6 col-sm-4 bg-primary text-dark lh-lg'>
-            <div><h2>Student</h2></div>
+            </p>
+      <a href="#" className="btn btn-primary">Go somewhere</a>
+  </div>
+</div>
+</div>
+<div className='col-xs-6 col-sm-4 text-dark fw-bolder'>
+      <div className="card  bg-info " style={{width: "25rem"}}>
+      <img src="https://icons.iconarchive.com/icons/graphicloads/100-flat/128/student-icon.png" className="card-img-top align-center" style={{width:"300px",height:"300px",alignSelf:"center"}} alt="..."/>
+      <div className="card-body">
+      <h5 className="card-title"><h2>Student</h2></h5>
+      <p className="card-text">
             <h3>
-    The Registered student can access the test and can track 
-    his performance of test of all subjects shown by the chart 
+            teacher/user can create a test of subject accordingly ,
+            he can get the results of all students after test complete
+            the Registered student can access the test and can track his 
+            performance of test of all subjects shown by the chart
             </h3>
+            </p>
+      <a href="#" className="btn btn-primary">Go somewhere</a>
+  </div>
+</div>
+</div>
       </div>
       </div>
- 
-      </div>
+     
       <br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>
       <div className="footer">
             <b><center>All Rights Reserverd @Cdac-March-2022</center></b>   
