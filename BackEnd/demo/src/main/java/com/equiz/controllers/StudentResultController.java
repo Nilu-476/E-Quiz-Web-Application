49df1c4d2e67b306eaa4087e1d10bca4446f5b5c
SharmaHemant23 <sharmashubham23694@gmail.com>
2022-09-20 23:40:23 -0700

    controller updated

--- BackEnd/demo/src/main/java/com/equiz/controllers/StudentResultController.java
+++ BackEnd/demo/src/main/java/com/equiz/controllers/StudentResultController.java
@@ -113,7 +113,29 @@ public class StudentResultController {
 		return l;
 	}
 	
-	
+	@GetMapping(value = "/deletestudentresult/{username}")
+	public String deleteStudentResult(@PathVariable String username)
+	{
+		StudentEntity s = studrepo.findByUsername(username);
+		
+		boolean flag = false;
+		
+		List<ResultEntity> results = resRepo.findAll();
+		for(ResultEntity r : results)
+		{
+			if(r.getStudent().getUsername().equals(s.getUsername()))
+			{
+				resRepo.delete(r);
+				flag = true;
+			}
+		
+		}
+		
+		if(flag)
+			return "Results deleted successfully ";
+		else
+			return "Results not deleted";
+	}
 	
 	
 	
